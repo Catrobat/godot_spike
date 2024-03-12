6bba419420647bfa465e525a0f80b3aceefed4cf
Dorian Percic <dorian.percic@gmail.com>
2024-03-12 17:55:48 +0100

    Rename button

--- godot/src/AddStatement.gd
+++ godot/src/AddStatement.gd
@@ -3,13 +3,9 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass  # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_pressed():
-	print("Pressed")  # Replace with function body.
