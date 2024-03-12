38cf631323d36c53f43c652eab8168cbda5733ff
Max Oberaigner <contact@oberaigner.info>
2024-03-12 11:12:37 +0100

    Project: Integrate & use Rust plugin from Godot

--- rust/godot-plugin/src/lib.rs
+++ rust/godot-plugin/src/lib.rs
@@ -1,14 +1,74 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use godot::engine::ISprite2D;
+use godot::engine::Sprite2D;
+use godot::prelude::*;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    speed: f64,
+    angular_speed: f64,
+
+    base: Base<Sprite2D>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Hello, world!"); // Prints to the Godot console
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Self {
+            speed: 400.0,
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        // GDScript code:
+        //
+        // rotation += angular_speed * delta
+        // var velocity = Vector2.UP.rotated(rotation) * speed
+        // position += velocity * delta
+
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
+
+        let rotation = self.base().get_rotation();
+        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
+        self.base_mut().translate(velocity * delta as f32);
+
+        // or verbose:
+        // let this = self.base_mut();
+        // this.set_position(
+        //     this.position() + velocity * delta as f32
+        // );
+    }
+}
+
+#[derive(GodotClass)]
+struct API {}
+
+#[godot_api]
+impl INode for API {
+    fn init(_base: Base<RefCounted>) -> Self {
+        API {}
+    }
+}
+
+#[godot_api]
+impl API {
+    #[func]
+    fn damage() -> i32 {
+        return 42;
+    }
+
+    #[func]
+    fn get_name() -> GString {
+        "Henlo".into()
     }
 }
