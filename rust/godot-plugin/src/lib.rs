3bf287ce213bafae2b67bbb4b528f925026c3918
Max Oberaigner <contact@oberaigner.info>
2024-03-12 11:55:24 +0100

    Godot/Rust API: Remove the test "Player" implementation

--- rust/godot-plugin/src/lib.rs
+++ rust/godot-plugin/src/lib.rs
@@ -1,51 +1,33 @@
 use godot::engine::Engine;
-use godot::engine::ISprite2D;
-use godot::engine::Sprite2D;
 use godot::prelude::*;
 
-#[derive(GodotClass)]
-#[class(base=Sprite2D)]
-struct Player {
-    speed: f64,
-    angular_speed: f64,
+// Register this plugin as a Godot Extension
+struct MyExtension {}
 
-    base: Base<Sprite2D>,
-}
-
-#[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {
+    fn on_level_init(level: InitLevel) {
+        if level == InitLevel::Scene {
+            Engine::singleton()
+                .register_singleton(StringName::from("API"), API::new_alloc().upcast());
         }
     }
 
-    fn physics_process(&mut self, delta: f64) {
-        // GDScript code:
-        //
-        // rotation += angular_speed * delta
-        // var velocity = Vector2.UP.rotated(rotation) * speed
-        // position += velocity * delta
+    fn on_level_deinit(level: InitLevel) {
+        if level == InitLevel::Scene {
+            let mut engine = Engine::singleton();
+            let singleton_name = StringName::from("API");
+            let singleton = engine
+                .get_singleton(singleton_name.clone())
+                .expect("cannot retrieve the engine singleton");
 
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
-
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        self.base_mut().translate(velocity * delta as f32);
-
-        // or verbose:
-        // let this = self.base_mut();
-        // this.set_position(
-        //     this.position() + velocity * delta as f32
-        // );
+            engine.unregister_singleton(singleton_name);
+            singleton.free();
+        }
     }
 }
 
+// Implementation of the API
 #[derive(GodotClass)]
 #[class(base=Object, init)]
 struct API {}
@@ -60,39 +42,5 @@ impl API {
     #[func]
     fn get_name() -> GString {
         "Henlo".into()
-    }
-}
-
-struct MyExtension {}
-
-#[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {
-    fn on_level_init(level: InitLevel) {
-        if level == InitLevel::Scene {
-            // The StringName identifies your singleton and can be
-            // used later to access it.
-            Engine::singleton()
-                .register_singleton(StringName::from("API"), API::new_alloc().upcast());
-        }
-    }
-
-    fn on_level_deinit(level: InitLevel) {
-        if level == InitLevel::Scene {
-            // Get the `Engine` instance and `StringName` for your singleton.
-            let mut engine = Engine::singleton();
-            let singleton_name = StringName::from("API");
-
-            // We need to retrieve the pointer to the singleton object,
-            // as it has to be freed manually - unregistering singleton
-            // doesn't do it automatically.
-            let singleton = engine
-                .get_singleton(singleton_name.clone())
-                .expect("cannot retrieve the singleton");
-
-            // Unregistering singleton and freeing the object itself is needed
-            // to avoid memory leaks and warnings, especially for hot reloading.
-            engine.unregister_singleton(singleton_name);
-            singleton.free();
-        }
     }
 }
