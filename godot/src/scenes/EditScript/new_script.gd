e2ba27538063bc83044e859a96cc14510dfc52e4
Max Oberaigner <contact@oberaigner.info>
2024-04-10 14:11:36 +0200

    Completely rework UI

--- godot/src/scenes/EditScript/new_script.gd
+++ godot/src/scenes/EditScript/new_script.gd
@@ -1,11 +1,16 @@
-extends HBoxContainer
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
+
+
+func setup(string: String):
+	var button: Button = get_node("Label")
+	button.text = string
