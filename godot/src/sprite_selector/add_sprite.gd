7158b55972bf1e9de86110969bd22a6798b4c7af
Alexander Rösel <alexander.roesel@student.tugraz.at>
2024-04-12 14:56:12 +0200

    scrollbar working

--- godot/src/sprite_selector/add_sprite.gd
+++ godot/src/sprite_selector/add_sprite.gd
@@ -20,4 +20,13 @@ func _on_add_sprite_button_pressed():
 func _on_file_dialog_file_selected(path):
 	var sprite: Button = Sprites.instantiate()
 	sprite.setup(path)
-	get_node("VBox").add_child(sprite)
+	var margin_container = MarginContainer.new()
+	var margin_value = 10
+	margin_container.add_theme_constant_override("margin_top", margin_value / 2)
+	margin_container.add_theme_constant_override("margin_left", margin_value)
+	margin_container.add_theme_constant_override("margin_bottom", margin_value / 2)
+	margin_container.add_theme_constant_override("margin_right", margin_value)
+	margin_container.add_child(sprite)
+	#get_node("VBox/ScrollContainer/VBoxContainer").add_child(margin_container)
+	get_node("VBoxContainer2/VBox/ScrollContainer/VBoxIntems").add_child(margin_container)
+	print_tree_pretty()
