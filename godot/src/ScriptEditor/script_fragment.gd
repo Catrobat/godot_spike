cac7b89ec68d0aad078de2139445e758e1fe29e6
Max Oberaigner <contact@oberaigner.info>
2024-04-16 12:59:45 +0200

    Roundtrip between Godot and Rust

--- godot/src/ScriptEditor/script_fragment.gd
+++ godot/src/ScriptEditor/script_fragment.gd
@@ -1,13 +1,33 @@
 extends Node
 
-const MoveFragment: Resource = preload("res://src/ScriptEditor/move_fragment.tscn")
+const MoveFragment: Resource = preload("res://src/ScriptEditor/MoveFragment.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().get_root().add_child(MoveFragment.instantiate())
+	GlobalSignals.connect("SCRIPT_UPDATED", _on_script_updated)
+
+	var ast = API.get_ast()
+	render_ast(ast)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_script_updated():
+	var ast = API.get_ast()
+	render_ast(ast)
+
+
+func render_ast(ast):
+	#get_node("/VBox").remove()
+	print_tree_pretty()
+
+	var code_container := $VBoxContainer/VScrollContainer/Code
+	for old in code_container.get_children():
+		code_container.remove_child(old)
+
+	for fragment in ast:
+		code_container.add_child(MoveFragment.instantiate())
