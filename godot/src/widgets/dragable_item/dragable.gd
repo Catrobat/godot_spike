b70fb2ca95c4ac8f28812d31bc819922df2b9b99
Alexander Rösel <alexander.roesel@student.tugraz.at>
2024-04-23 14:37:25 +0200

    added missing files

---
+++ godot/src/widgets/dragable_item/dragable.gd
@@ -0,0 +1,39 @@
+@tool
+class_name DragAndDropItem
+extends Button
+
+@onready var target = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(_on_pressed)  # Replace with function body.
+
+
+func _on_pressed():
+	assert(false, "methon not implemented")
+
+
+func _get_drag_data(_at_position: Vector2):
+	set_drag_preview(get_preview_controll())
+	target = get_parent().get_node(self.get_path()).get_index(true)
+	return self
+
+
+func _can_drop_data(_at_position, data):
+	target = get_parent().get_node(self.get_path()).get_index(true)
+	var node = get_parent().get_node(data.get_path())
+	get_parent().move_child(node, target)
+	return true
+
+
+func get_preview_controll():
+	return duplicate()
+
+
+func _drop_data(_at_position, data):
+	var node = get_parent().get_node(data.get_path())
+	get_parent().move_child(node, target)
+
+#func _on_pressed():
+#	get_tree().change_scene_to_file("res://src/script_editor/script_fragment.tscn")
