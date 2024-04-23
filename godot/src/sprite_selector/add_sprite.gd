58396799750218a6f835d465d22fb2c1b2d96d72
ARoesel <90780644+KarlDerMaisbauer@users.noreply.github.com>
2024-04-23 11:01:18 +0200

    41 sprites drag and drop (#67)
    Co-authored-by: Alexander Rösel <alexander.roesel@student.tugraz.at>

--- godot/src/sprite_selector/add_sprite.gd
+++ godot/src/sprite_selector/add_sprite.gd
@@ -5,7 +5,7 @@ var Sprites: Resource = preload("res://src/sprite_selector/sprite.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_on_file_dialog_file_selected("Default Sprite")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +20,11 @@ func _on_add_sprite_button_pressed():
 func _on_file_dialog_file_selected(path):
 	var sprite: Button = Sprites.instantiate()
 	sprite.setup(path)
-	get_node("VBox").add_child(sprite)
+	var margin_container = MarginContainer.new()
+	var margin_value = 10
+	margin_container.add_theme_constant_override("margin_top", margin_value / 2)
+	margin_container.add_theme_constant_override("margin_left", margin_value)
+	margin_container.add_theme_constant_override("margin_bottom", margin_value / 2)
+	margin_container.add_theme_constant_override("margin_right", margin_value)
+	get_node("VBoxContainer2/VBox/ScrollContainer/VBoxIntems").add_child(sprite)
+	print_tree_pretty()
