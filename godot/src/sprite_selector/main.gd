7b720e9df9723f6848042defdbeeb66448780781
Alexander Rösel <alexander.roesel@student.tugraz.at>
2024-04-23 14:26:08 +0200

    help i have to rebase because max has pushed something which i have merged

--- godot/src/sprite_selector/main.gd
+++ godot/src/sprite_selector/main.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var Sprites: Resource = preload("res://src/sprite_selector/sprite.tscn")
+var Sprites: Resource = preload("res://src/sprite_selector/sprite_test.tscn")
 
 
 func _on_add_sprite_button_pressed():
@@ -8,12 +8,13 @@ func _on_add_sprite_button_pressed():
 
 
 func _on_file_dialog_file_selected(path):
-	var sprite: Button = Sprites.instantiate()
+	var sprite = Sprites.instantiate()
 	sprite.setup(path)
 	var margin_container = MarginContainer.new()
-	var margin_value = 10
-	margin_container.add_theme_constant_override("margin_top", margin_value / 2)
-	margin_container.add_theme_constant_override("margin_left", margin_value)
-	margin_container.add_theme_constant_override("margin_bottom", margin_value / 2)
-	margin_container.add_theme_constant_override("margin_right", margin_value)
+	#var margin_value = 10
+	#margin_container.add_theme_constant_override("margin_top", margin_value / 2)
+	#margin_container.add_theme_constant_override("margin_left", margin_value)
+	#margin_container.add_theme_constant_override("margin_bottom", margin_value / 2)
+	#margin_container.add_theme_constant_override("margin_right", margin_value)
+	#get_node("VBoxContainer2/VBox/ScrollContainer/VBoxIntems").add_child(sprite)
 	get_node("VBoxContainer2/VBox/ScrollContainer/VBoxIntems").add_child(sprite)
